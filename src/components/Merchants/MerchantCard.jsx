9a0977294a11c85ca03ed1c34c1de5d1ae1b14fd
Hozan Saeed <hozan.saeed.19@gmail.com>
2018-08-14 17:56:40 +0300

    merchant card

--- src/components/Merchants/MerchantCard.jsx
+++ src/components/Merchants/MerchantCard.jsx
@@ -1,10 +1,7 @@
 import React, {Component} from 'react'
-import {Card, Icon, Image, Grid} from 'semantic-ui-react'
+import {Card, Icon, Image, Grid, Label} from 'semantic-ui-react'
 import FittedImg from 'react-fitted-img';
 
-  const merchantCardStyle={
-
-  }
 class MerchantCard extends Component {
   constructor(props) {
     super(props)
@@ -15,24 +12,26 @@ class MerchantCard extends Component {
     <Card fluid="fluid" href={"../MerchantMenus/"+merchant.merchant_id}>
       <FittedImg fit="cover" alt="food" src={merchant.merchant_image} height="300"/>
       <Card.Content>
-      <Grid Column={2}>
-        <Grid.Row>
-          <Grid.Column>
-            <h1>{merchant.merchant_name.en}</h1>
-          </Grid.Column>
-          <Grid.Column>
-            <Image src={merchant.merchant_logo}/>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column>
-            <Card.Description>{merchant.merchant_open_time} - {merchant.merchant_close_time}</Card.Description>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-
-      </Card.Content>
-      <Card.Content extra>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column>
+              <Card.Header>
+                <h2>{merchant.merchant_name.en}</h2>
+              </Card.Header>
+            </Grid.Column>
+            <Grid.Column>
+              <Image src={merchant.merchant_logo}/>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <Card.Description >
+                <Label style={{background:'#f5f5f5',fontSize:15}}>{merchant.merchant_open_time} - {merchant.merchant_close_time}</Label>
+                <Label style={{background:'#f5f5f5',fontSize:15}}>This is Test</Label>
+              </Card.Description>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </Card.Content>
     </Card>)
   }
